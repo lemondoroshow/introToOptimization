9522e4a02dbd76821ea6ea66f5b1f79ed02117b6
Lemon Doroshow <isaacdoroshow@gmail.com>
2025-09-14 14:47:14 -0400

    Optimize constraints for price

--- HW2/problem1.m
+++ HW2/problem1.m
@@ -26,3 +26,31 @@ c2 = [6.72 3.19 2.69 7.29 500]
 % Solve new linear program & obj func
 x2 = linprog(c2, A, b, [], [], zeros(5, 1), [])
 obj2 = c2 * x2
+
+% Attempt 1 to adjust b to reduce cost
+b2 = [-1500; -275; -50; 78; 300; 2300]
+x3 = linprog(c2, A, b2, [], [], zeros(5, 1), [])
+obj3 = c2 * x3
+
+% Attempt 2 to adjust b to reduce cost
+b3 = [-1500; -150; -50; 78; 300; 2300]
+x4 = linprog(c2, A, b3, [], [], zeros(5, 1), [])
+obj4 = c2 * x4
+
+% Attempt 3 to adjust b to reduce cost
+b4 = [-1000; -150; -50; 78; 300; 2300]
+x5 = linprog(c2, A, b4, [], [], zeros(5, 1), [])
+obj5 = c2 * x5
+
+% Attempt 4 to adjust b to reduce cost
+b5 = [-1000; -150; -50; 78; 300; 3200]
+x6 = linprog(c2, A, b5, [], [], zeros(5, 1), [])
+obj5 = c2 * x6
+
+% Attempt 5 to adjust b to reduce cost
+b6 = [-1000; -150; -50; 95; 300; 3200]
+x7 = linprog(c2, A, b6, [], [], zeros(5, 1), [])
+obj5 = c2 * x7
+
+% Close diary
+diary off
