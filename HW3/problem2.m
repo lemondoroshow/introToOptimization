0216416f580f1cf1136bed537d20ae1b9e8c943b
Lemon Doroshow <isaacdoroshow@gmail.com>
2025-09-19 17:10:47 -0400

    Loop through all possible candidate BFVs

--- HW3/problem2.m
+++ HW3/problem2.m
@@ -9,5 +9,21 @@ A = [-0.27 -0.12 -0.045 1 0 0 0;
      0 0 1 0 0 0 1]
 c = [-200.2; -50.2; -25.2; 0; 0; 0; 0]
 
-% Row reduce A (yes, it's technically already reduced; still fixes order)
-A = rref(A)
+% Loop through all row combinations of A
+combos = nchoosek(1:6, 4)
+successes = 0 % Debugging
+for i = 1:size(combos, 1)
+
+    % Filter A and reduce
+    combo = combos(i,1:size(combos, 2))
+    A_selected = rref(A(1:size(A, 1), combo))
+
+    % If reduced columns are lin indep, continue
+    if A_selected == eye(4)
+        success = true
+        successes = successes + 1 % Debugging
+            
+    else
+        success = false
+    end
+end
