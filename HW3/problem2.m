4a8c96b86e84c47a79bc1cf4d100044d32d85f9b
Lemon Doroshow <isaacdoroshow@gmail.com>
2025-09-19 17:23:03 -0400

    Attempt 1 to loop through BFVs w/ matrix combinations

--- HW3/problem2.m
+++ HW3/problem2.m
@@ -12,6 +12,7 @@ c = [-200.2; -50.2; -25.2; 0; 0; 0; 0]
 % Loop through all row combinations of A
 combos = nchoosek(1:6, 4)
 successes = 0 % Debugging
+all_bfvs = []
 for i = 1:size(combos, 1)
 
     % Filter A and reduce
@@ -20,10 +21,21 @@ for i = 1:size(combos, 1)
 
     % If reduced columns are lin indep, continue
     if A_selected == eye(4)
-        success = true
         successes = successes + 1 % Debugging
-            
+        
+        % Find c_b by multiplying selected values by A_inverse
+        A_selected_inv = inv(A(1:size(A, 1), combo))
+        c_b = A_selected_inv * c(combo, 1:size(c, 2))
+
+        % Create copy of c, replace w/ c_b and supplement w/ zeros
+        c_new = c
+        c_new(combo) = c_b
+        c_new(~combo) = 0
+        
+        % Add to total array
+        all_bfvs = [all_bfvs c_new]
+
     else
-        success = false
+        
     end
 end
