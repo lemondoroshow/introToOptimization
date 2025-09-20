0cd6ac7611108115c1ebe7c79fa92d783e6c71fc
Lemon Doroshow <isaacdoroshow@gmail.com>
2025-09-20 15:54:36 -0400

    Fix matrix size issues by un-hard-coding

--- HW3/problem2.m
+++ HW3/problem2.m
@@ -2,40 +2,45 @@
 echo on
 diary HW3prob2.txt
 
-% Define A and c from solution
+% Define A and c from solution, set up "c-zero" vector
 A = [-0.27 -0.12 -0.045 1 0 0 0; 
      -1 -0.75 -0.2 0 1 0 0; 
      -2 1 0 0 0 1 0;
      0 0 1 0 0 0 1]
 c = [-200.2; -50.2; -25.2; 0; 0; 0; 0]
+c_z = zeros(size(c, 1), 1)
 
 % Loop through all row combinations of A
-combos = nchoosek(1:6, 4)
+combos = nchoosek(1:size(A, 2), size(A, 1))
 successes = 0 % Debugging
 all_bfvs = []
 for i = 1:size(combos, 1)
 
-    % Filter A and reduce
+    % Filter A
     combo = combos(i,1:size(combos, 2))
-    A_selected = rref(A(1:size(A, 1), combo))
+    A_selected = A(1:size(A, 1), combo)
 
     % If reduced columns are lin indep, continue
-    if A_selected == eye(4)
+    if rref(A_selected) == eye(size(A, 1))
         successes = successes + 1 % Debugging
         
         % Find c_b by multiplying selected values by A_inverse
         A_selected_inv = inv(A(1:size(A, 1), combo))
         c_b = A_selected_inv * c(combo, 1:size(c, 2))
 
-        % Create copy of c, replace w/ c_b and supplement w/ zeros
-        c_new = c
+        % Create zero vector, replace values
+        c_new = c_z
         c_new(combo) = c_b
-        c_new(~combo) = 0
         
-        % Add to total array
-        all_bfvs = [all_bfvs c_new]
+        % Add to total if no negatives
+        if any(c_new < 0) ~= 1
+            all_bfvs = [all_bfvs; c_new']
+        end
 
     else
         
     end
 end
+
+% End diary
+diary off
