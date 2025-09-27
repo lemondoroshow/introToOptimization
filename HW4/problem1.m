6d742798f6da72d20bbb3d339619e2bb1ff7324a
Lemon Doroshow <isaacdoroshow@gmail.com>
2025-09-27 13:23:16 -0400

    Implement trial for finding reduced costs and obj. func values

--- HW4/problem1.m
+++ HW4/problem1.m
@@ -1,4 +1,4 @@
-% Define A and c from solution, set up "c-zero" vector
+% Define A, b, c
 A = [0.27 0.12 0.045 1 0 0 0; 
      1 0.75 0.2 0 1 0 0; 
      2 -1 0 0 0 1 0;
@@ -6,25 +6,39 @@ A = [0.27 0.12 0.045 1 0 0 0;
 b = [100; 480; 0; 300]
 c = [-200.2; -50.2; -25.2; 0; 0; 0; 0]
 bfvs = []
+ofvs = []
+r_nTs = []
 
 % Iterate
-for i=1:7
-    for j=(i+1):7
-        for k=(j+1):7
-            for u=(k+1):7
+for i = 1:7
+    for j = (i + 1):7
+        for k = (j + 1):7
+            for u = (k + 1):7
                     
                     % Isolate matrix, calculate x_b
-                    B = A(:,[i j k u])
-                    x_b = B\b
+                    B = A(:, [i j k u])
+                    N = A(:, setdiff(1:7, [i j k u]))
+                    x_b = B \ b
 
                     if all(x_b >= 0)
                         
                         % Calculate new BFV
                         new_vector = [0; 0; 0; 0; 0; 0; 0]
-                        new_vector([i j k u]) = x_b
+                        new_vector([i j k u],:) = x_b
 
-                        % Add to collection
+                        % Calculate objective function value
+                        c_b = c([i j k u],:)
+                        ofv = c_b' * x_b
+
+                        % Calculate reduced costs
+                        c_n = c(setdiff(1:7, [i j k u]),:)
+                        r_n = (c_n' - c_b' * inv(B)  * N)'
+
+                        % Add to collections
                         bfvs = [bfvs; new_vector']
+                        ofvs = [ofvs; ofv]
+                        r_nTs = [r_nTs; r_n']
+                        
                     end
 
             end
