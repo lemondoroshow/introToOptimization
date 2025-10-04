df43e2f17c747a40766fd42db03b80a408df6514
Lemon Doroshow <isaacdoroshow@gmail.com>
2025-10-04 15:10:01 -0400

    Test + fix first tableau construction

--- HW5/simplex_method.m
+++ HW5/simplex_method.m
@@ -1,7 +1,13 @@
+% For my sake, turn off scientific notation
+format shortG
+
 % Import / set data
-A = []
-b = []
-c = []
+A = [0.27 0.12 0.045 1 0 0 0; 
+     1 0.75 0.2 0 1 0 0; 
+     2 -1 0 0 0 1 0;
+     0 0 1 0 0 0 1]
+b = [100; 480; 0; 300]
+c = [-200.2; -50.2; -25.2; 0; 0; 0; 0]
 basis_selection = [1 2 3 4]
 
 % Split constraints
@@ -19,10 +25,10 @@ invB_N = inv(B) * N
 x_b = inv(B) * b
 
 % Get top-row tableau values
-zero_row = zeroes(1, size(basis_selection, 2))
+zero_row = zeros(1, size(basis_selection, 2))
 r_nT = -c_n' + c_b' * invB_N
 ofv = c_b' * x_b
 
 % Construct tableau
-basic_tableau = [1 zero_row r_nT ofv;
-                 zero_col I invB_N x_b]
+basic_tableau = [1, zero_row, r_nT, ofv;
+                 zero_col, I, invB_N, x_b]
