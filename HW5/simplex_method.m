0bfb673d5265a9c585d541b8bf3120410707f311
Lemon Doroshow <isaacdoroshow@gmail.com>
2025-10-04 16:59:27 -0400

    Implement simplex method w/o solution for stalling

--- HW5/simplex_method.m
+++ HW5/simplex_method.m
@@ -2,33 +2,90 @@
 format shortG
 
 % Import / set data
-A = [0.27 0.12 0.045 1 0 0 0; 
-     1 0.75 0.2 0 1 0 0; 
-     2 -1 0 0 0 1 0;
-     0 0 1 0 0 0 1]
-b = [100; 480; 0; 300]
-c = [-200.2; -50.2; -25.2; 0; 0; 0; 0]
-basis_selection = [1 2 3 4]
+A = [];
+b = [];
+c = [];
+basis_selection = [];
 
 % Split constraints
-B = A(:, basis_selection)
-N = A(:, setdiff(1:size(A, 2), basis_selection))
+B = A(:, basis_selection);
+N = A(:, setdiff(1:size(A, 2), basis_selection));
 
 % Split obj func
-c_b = c(basis_selection, :)
-c_n = c(setdiff(1:size(A, 2), basis_selection),:)
+c_b = c(basis_selection, :);
+c_n = c(setdiff(1:size(A, 2), basis_selection),:);
 
 % Get bottom-row tableau values
-zero_col = zeros(size(basis_selection, 2), 1)
-I = eye(size(basis_selection, 2))
-invB_N = inv(B) * N
-x_b = inv(B) * b
+zero_col = zeros(size(basis_selection, 2), 1);
+I = eye(size(basis_selection, 2));
+invB_N = B \ N;
+x_b = B \ b;
 
 % Get top-row tableau values
-zero_row = zeros(1, size(basis_selection, 2))
-r_nT = -c_n' + c_b' * invB_N
-ofv = c_b' * x_b
+zero_row = zeros(1, size(basis_selection, 2));
+r_nT = -c_n' + c_b' * invB_N;
+ofv = c_b' * x_b;
 
 % Construct tableau
 basic_tableau = [1, zero_row, r_nT, ofv;
                  zero_col, I, invB_N, x_b]
+
+% Start loop until all reduced costs are positive
+loop_no = 1;
+while any(r_nT > 0)
+    
+    % Find greatest reduced cost
+    [temp_row, pivot_col] = find(basic_tableau == max(r_nT));
+
+    % Loop through pivot column rows to find lowest positive ratio
+    last_col = size(basic_tableau, 2);
+    pivot_ratios = [];
+    for i = 2:size(basic_tableau, 1)
+        
+        % Ensure ratio is non-negative
+        % WARNING: MAY STALL
+        pivot_ratio = basic_tableau(i, last_col) / ...
+                basic_tableau(i, pivot_col);
+        if pivot_ratio >= 0
+            
+            % Add to collection, check if its the lowest
+            pivot_ratios = [pivot_ratios pivot_ratio];
+
+            % If lowest positive ratio, set pivot_row to i
+            if pivot_ratio == min(pivot_ratios)
+                pivot_row = i;
+            end
+        end
+    end
+    
+    %{
+    comparison = basic_tableau(2:end, [pivot_col last_col]);
+    comparison = comparison(:, 2) ./ comparison(:, 1);
+
+    % Find lowest positive ratio
+    comparison = comparison(comparison > 0);
+    pivot_val = min(comparison);
+
+    % Find index in tablaeau
+    [pivot_row, temp_col] = find(basic_tableau(:, last_col) == pivot_val);
+    %}
+
+    % Use row operations to ensure pivot point == 1
+    basic_tableau(pivot_row, :) = basic_tableau(pivot_row, :) ...
+         .* 1 / basic_tableau(pivot_row, pivot_col);
+    
+    % Reduce rows
+    for irow = setdiff(1:size(basic_tableau, 1), pivot_row)
+        basic_tableau(irow, :) = basic_tableau(irow, :) ...
+            - basic_tableau(irow, pivot_col) ...
+            * basic_tableau(pivot_row, :);
+    end
+
+    % Debugging 
+    basic_tableau
+    
+    % Define new reduced costs
+    r_nT = basic_tableau(1, 2:(last_col - 1));
+    r_nT = r_nT(r_nT ~= 0)
+    
+end
