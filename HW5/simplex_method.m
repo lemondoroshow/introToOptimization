fe931af69338304125c95ea3855cfde94fd03679
Lemon Doroshow <isaacdoroshow@gmail.com>
2025-10-05 19:15:07 -0400

    Implement proper anti-stall code

--- HW5/simplex_method.m
+++ HW5/simplex_method.m
@@ -1,58 +1,86 @@
-% Turn on diary
-diary HW5prob5.txt
+%% Create pre-tableau
 
 % For my sake, turn off scientific notation
 format shortG
 
 % Import / set data
-load('prob5datafile.mat')
+load('prob1datafile.mat')
 disp(A)
 disp(b)
 disp(c)
-basis_selection = 4:7;
+basis = 1:3;
 
-% Split constraints
-B = A(:, basis_selection);
-N = A(:, setdiff(1:size(A, 2), basis_selection));
+% Arrange pre-tableau
+pretableau = [1, -1*c', 0;
+              zeros(size(A,1),1), A, b]
 
-% Split obj func
-c_b = c(basis_selection, :);
-c_n = c(setdiff(1:size(A, 2), basis_selection),:);
+%% Create initial tableau
 
-% Get bottom-row tableau values
-zero_col = zeros(size(basis_selection, 2), 1);
-I = eye(size(basis_selection, 2));
-invB_N = B \ N;
-x_b = B \ b;
+% Iterate through selected basis
+k = 2;
+for i = basis
 
-% Get top-row tableau values
-zero_row = zeros(1, size(basis_selection, 2));
-r_nT = -c_n' + c_b' * invB_N;
-ofv = c_b' * x_b;
+    % Make basic variable in first row 1
+    irow = pretableau(k, :);
+    pretableau(k, :) = 1 / irow(1, i + 1) * irow;
 
-% Construct tableau
-basic_tableau = [1, zero_row, r_nT, ofv;
-                 zero_col, I, invB_N, x_b];
-disp(basic_tableau)
+    % Subtract row from other rows
+    for j = 1:size(pretableau, 1)
+        if j ~= k
+            
+            % Row operations
+            pretableau(j, :) = pretableau(j, :) ...
+                - pretableau(j, i + 1) / irow(1, i + 1) * irow;
+        end
+    end
 
-% Start loop until all reduced costs are positive
-loop_no = 1; % Debugging
-disp(loop_no)
-while any(r_nT > 0) & loop_no <= 1000 % Just in case of cycling
-    
+    % Increment k
+    k = k + 1;
+end
+
+tableau = pretableau
+
+%% Find initial reduced costs
+
+neg_r_nT = [];
+neg_r_nT_i = [];
+for ind = 2:size(tableau, 2) - 1
+    col = tableau(:, ind);
+
+    % Check if column is standard basis
+    if ~any(abs(col(col ~= 0) - 1) > 0) && ...
+            sum(col ~= 0) == 1
+
+        % Do nothing
+
+    else 
+        neg_r_nT = [neg_r_nT; tableau(1, ind)];
+        neg_r_nT_i = [neg_r_nT_i; ind];
+    end
+end
+
+% Create "dictionary" for reduced costs
+neg_r_nT_i = [neg_r_nT neg_r_nT_i];
+
+%% Loop through simplex method
+
+% Iterate while any reduced costs are non-negative
+while any(neg_r_nT >= 0)
+
     % Find greatest reduced cost
-    pivot_col = find(abs(basic_tableau(1, 2:end) - max(r_nT)) < 0.0001) + 1;
-
-    % Loop through pivot column rows to find lowest positive ratio
-    last_col = size(basic_tableau, 2);
+    [~, pos] = max(neg_r_nT_i);
+    pivot_col = neg_r_nT_i(pos(1), 2);
+    
+    % Find lowest positive ratio
+    last_col = size(tableau, 2);
     pivot_ratios = [];
-    for i = 2:size(basic_tableau, 1)
+    for i = 2:size(tableau, 1)
         
-        % Ensure ratio is non-negative
-        pivot_ratio = basic_tableau(i, last_col) / ...
-                basic_tableau(i, pivot_col);
-        if pivot_ratio ~= 0 && ...
-                basic_tableau(i, last_col) > 0
+        % Ensure each part is non-negative
+        pivot_ratio = tableau(i, last_col) / ...
+                tableau(i, pivot_col);
+        if tableau(i, last_col) >= 0 && ...
+                tableau(i, pivot_col) >= 0
             
             % Add to collection, check if its the lowest
             pivot_ratios = [pivot_ratios pivot_ratio];
@@ -63,83 +91,45 @@ while any(r_nT > 0) & loop_no <= 1000 % Just in case of cycling
             end
         end
     end
-
+    
     % Use row operations to ensure pivot point == 1
-    basic_tableau(pivot_row, :) = basic_tableau(pivot_row, :) ...
-         .* 1 / basic_tableau(pivot_row, pivot_col);
-    
+    tableau(pivot_row, :) = tableau(pivot_row, :) ...
+         .* 1 / tableau(pivot_row, pivot_col);
+
     % Reduce rows
-    for irow = 1:size(basic_tableau, 1)
-        if (basic_tableau(irow, last_col) ~= 0 ... % Prevent stalling
-            && irow ~= pivot_row) ... % Don't operate on pivot row
-            || irow == 1 % Ensure top row is included, even if OFV == 0
+    for irow = 1:size(tableau, 1)
+        if irow ~= pivot_row % Don't operate on pivot row
+            
+            % Row operations
+            tableau(irow, :) = tableau(irow, :) ...
+                - tableau(irow, pivot_col) ...
+                * tableau(pivot_row, :);
+        end
+    end
 
-            basic_tableau(irow, :) = basic_tableau(irow, :) ...
-                - basic_tableau(irow, pivot_col) ...
-                * basic_tableau(pivot_row, :);
+    % Dispay tableau
+    disp(tableau)
+    
+    % Find next reduced costs
+    neg_r_nT = [];
+    neg_r_nT_i = [];
+    for ind = 2:size(tableau, 2) - 1
+        col = tableau(:, ind);
 
-        elseif basic_tableau(irow, last_col) ... % Account for weird edge-
-                - basic_tableau(irow, pivot_col) ... % case
-                * basic_tableau(pivot_row, last_col) > 0
+        % Check if column is standard basis
+        if ~any(abs(col(col ~= 0) - 1) > 0) && ...
+               sum(col ~= 0) == 1
 
-            basic_tableau(irow, :) = basic_tableau(irow, :) ...
-                - basic_tableau(irow, pivot_col) ...
-                * basic_tableau(pivot_row, :);
+            % Do nothing
+
+        else 
+            neg_r_nT = [neg_r_nT; tableau(1, ind)];
+            neg_r_nT_i = [neg_r_nT_i; ind];
         end
     end
     
-    % Display tableau
-    disp(basic_tableau)
-
-    % Define new reduced costs
-    r_nT = basic_tableau(1, 2:(last_col - 1));
-    r_nT = r_nT(r_nT ~= 0);
-
-    % Debugging
-    loop_no = loop_no + 1;
-    disp(loop_no)
+    % Create "dictionary" for reduced costs
+    neg_r_nT_i = [neg_r_nT neg_r_nT_i];
 end
 
-% Loop through rows to find x-indices
-var_row = [];
-for irow = 2:size(basic_tableau, 1)
-    for icol = 2:size(basic_tableau, 2)
-
-        % Get matrix value
-        val = basic_tableau(irow, icol);
-
-        % Check that column has one 1, rest 0s
-        ovic = basic_tableau(setdiff(1:size(basic_tableau, 1), irow), icol);
-        if val == 1 && ... 
-                all(ovic == 0)
-                
-            % Store which row the variable is in  
-            var_row = [var_row; [icol - 1, irow]];
-        end
-    end
-end
-
-% Check if simplex method converged
-if loop_no < 1000
-
-    % Form x vector
-    % Still unsure if this is correct, but I don't know why it wouldn't be
-    x = zeros(size(A, 2), 1);
-    for var = 1:size(var_row, 1)
-    
-        % Set x at the position of the variable to the optimal vector value
-        x(var_row(var, 1), :) = basic_tableau(var_row(var, 2), last_col);
-    end
-
-    % Isolate OFV
-    ofv = basic_tableau(1, last_col);
-
-    % Output optimal x, OFV
-    disp("Optimal x")
-    disp(x)
-    disp("Optimal OFV")
-    disp(ofv)
-end
-
-% Turn off diary
-diary off
+%%
