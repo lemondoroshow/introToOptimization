795026c3423ae7d1cb9cfeba3e17fc019c1a59d6
Lemon Doroshow <isaacdoroshow@gmail.com>
2025-10-05 13:24:30 -0400

    Verify solution to problem 1

---
+++ HW5/simplex_verification.m
@@ -0,0 +1,42 @@
+% Define A, b, c
+load("prob1datafile.mat")
+bfvs = []
+ofvs = []
+r_nTs = []
+selected_columns = []
+
+% Iterate
+for i = 1:7
+    for j = (i + 1):7
+        for k = (j + 1):7
+                    
+                    % Isolate matrix, calculate x_b
+                    B = A(:, [i j k])
+                    N = A(:, setdiff(1:7, [i j k]))
+                    x_b = B \ b
+
+                    if all(x_b >= 0)
+                        
+                        % Calculate new BFV
+                        new_vector = [0; 0; 0; 0; 0; 0; 0]
+                        new_vector([i j k],:) = x_b
+
+                        % Calculate objective function value
+                        c_b = c([i j k],:)
+                        ofv = c_b' * x_b
+
+                        % Calculate reduced costs
+                        c_n = c(setdiff(1:7, [i j k]),:)
+                        r_n = (c_n' - c_b' * inv(B)  * N)'
+
+                        % Add to collections
+                        bfvs = [bfvs; new_vector']
+                        ofvs = [ofvs; ofv]
+                        r_nTs = [r_nTs; r_n']
+                        selected_columns = [selected_columns; [i j k]]
+                        
+                    end
+
+        end
+    end
+end
