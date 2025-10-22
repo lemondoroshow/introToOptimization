d85096bf4c6debcba8b39333247d3e231b360e3b
Lemon Doroshow <isaacdoroshow@gmail.com>
2025-10-22 14:23:39 -0400

    Complete first sudoku attempt

--- HW7/sudoku.m
+++ HW7/sudoku.m
@@ -1,4 +1,4 @@
-%% Boilerplace
+%% Boilerplate
 
 function [position] = indexToPosition(i, j, k, l, m)
 
@@ -20,10 +20,13 @@ M = [8 0 0 0 0 0 0 0 0;
      0 0 8 5 0 0 0 1 0;
      0 9 0 0 0 0 4 0 0];
 
+% Initialize b
+b = ones(324, 1);
+
 %% Find objective function (c)
 
 % Initialize c
-c = zeros(size(M, 1) * size(M, 2) * 9, 1);
+c = ones(729, 1);
 
 % Loop through M to find values for c
 for i = 1:3
@@ -42,10 +45,103 @@ for i = 1:3
                     % Add 1 to c in position
                     m = entry;
                     pos = indexToPosition(i, j, k, l, m);
-                    c(pos) = 1;
+                    c(pos) = 0;
 
                 end
             end
         end
     end
 end
+
+%% Define constraint matrix (A)
+
+% Set initial matrix
+A = zeros(324, 729);
+A_row = 0;
+
+% Set 1-integer constraints
+for i = 1:3
+    for k = 1:3
+        for j = 1:3
+            for l = 1:3
+                
+                % Increment row of A to operate on
+                A_row = A_row + 1;
+                
+                % Summation
+                for m = 1:9
+
+                    % Set A equal to 1 in correct positions
+                    A(A_row, indexToPosition(i, j, k, l, m)) = 1;
+
+                end
+            end
+        end
+    end
+end
+
+% Set large-square constraints
+for i = 1:3
+    for j = 1:3
+        for m = 1:9
+
+            % Increment row of A to operate on
+            A_row = A_row + 1;
+
+            % Summation
+            for k = 1:3
+                for l = 1:3
+
+                    % Set A equal to 1 in correct positions
+                    A(A_row, indexToPosition(i, j, k, l, m)) = 1;
+
+                end
+            end
+        end
+    end
+end
+
+% Set row constraints
+for i = 1:3
+    for k = 1:3
+        for m = 1:9
+
+            % Increment row of A to operate on
+            A_row = A_row + 1;
+
+            % Summation
+            for j = 1:3
+                for l = 1:3
+
+                    % Set A equal to 1 in correct positions
+                    A(A_row, indexToPosition(i, j, k, l, m)) = 1;
+
+                end
+            end
+        end
+    end
+end
+
+% Set column constraints
+for j = 1:3
+    for l = 1:3
+        for m = 1:9
+
+            % Increment row of A to operate on
+            A_row = A_row + 1;
+
+            % Summation
+            for i = 1:3
+                for k = 1:3
+
+                    % Set A equal to 1 in correct positions
+                    A(A_row, indexToPosition(i, j, k, l, m)) = 1;
+
+                end
+            end
+        end
+    end
+end
+
+%% Solve linear program
+x = intlinprog(c, 1:324, [], [], A, b);
