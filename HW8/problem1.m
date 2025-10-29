d7ff1634ed0d58c48e02b03339bd053c0f4451ce
Lemon Doroshow <isaacdoroshow@gmail.com>
2025-10-29 19:53:38 -0400

    Implement both trials for problem 1

---
+++ HW8/problem1.m
@@ -0,0 +1,47 @@
+%% First trial
+
+% Set matrices
+B = [1, 2;
+     2, 4];
+A = B + 7 * eye(2)
+
+% Get and display eigenvalues
+eigen_B = eig(B)
+eigen_A = eig(A)
+
+% Get 100 random vectors
+v = [];
+for i = 1:100
+    d = randn(2, 1);
+    
+    % Calculate quantity and add to v
+    v = [v, 1 / (d' * d) * d' * A * d];
+    
+end
+
+% Plot v
+plot(v, zeros(1, 100), 'r.')
+
+%% Second trial
+
+% Set matrices
+B = [1, 2;
+     2, 4];
+A = 2 * B + 3 * eye(2)
+
+% Get and display eigenvalues
+eigen_B = eig(B)
+eigen_A = eig(A)
+
+% Get 100 random vectors
+v = [];
+for i = 1:100
+    d = randn(2, 1);
+    
+    % Calculate quantity and add to v
+    v = [v, 1 / (d' * d) * d' * A * d];
+    
+end
+
+% Plot v
+plot(v, zeros(1, 100), 'r.')
