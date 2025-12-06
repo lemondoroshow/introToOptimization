e41c9a9ee17eeb7518040398fc7b72d30e5d577d
Lemon Doroshow <isaacdoroshow@gmail.com>
2025-12-06 16:46:51 -0500

    Refactor QuadOpti and solve problem 1

--- HW10/quadratic.m
+++ HW10/quadratic.m
@@ -1,7 +1,8 @@
-%% Set up problem
-
 % Turn off scientific notation
 format shortG
+
+% Turn on diary
+diary HW10prob1.txt
 
 % Define data
 Q = [4 1 1 1;
@@ -13,96 +14,100 @@ A = [1 0 -2 1;
      0 -2 1 -3];
 b = [-1; 2];
 
-% Get sizes
-n = size(Q, 1);
-m = size(b, 1);
+function [x, y, z, k] = QuadraticOptimization(Q, c, A, b, show)
 
-% Initialize values of x, y, z, delta, epsilons
-% These are basically entirely test values or given
-eps_min = 0.1;
-eps_max = 0.9;
-delta = 0.5;
-x = ones(n, 1);
-y = ones(m, 1);
-z = ones(n, 1);
-
-% Compile vector
-xyz = [x; y; z];
-disp('Problem initialized')
-
-%% Solve problem
-
-% Iterate until done
-finished = false;
-k = 0;
-while ~finished
+    % Get sizes
+    n = size(Q, 1);
+    m = size(b, 1);
     
-    % Iterate trial
-    k = k + 1;
-
-    % Get epsilon value based on parity
-    eps = mod(k, 2) * eps_min + abs(1 - mod(k, 2)) * eps_max;
-
-    % Find avg complementarity
-    beta = (z' * x) / n;
-
-    % Find F of current vector subject to beta-epsilon
-    F = [-Q * x - c + A' * y + z;
-         A * x - b;
-         (z .* x) - (ones(n, 1) * eps * beta)];
-
-    % Find gradient of F
-    grad_F = [-Q, A', eye(n);
-              A, zeros(m, m), zeros(m, n);
-              diag(z), zeros(n, m), diag(x)];
+    % Initialize values of x, y, z, delta, epsilons
+    % These are basically entirely test values or given
+    eps_min = 0.1;
+    eps_max = 0.9;
+    delta = 0.5;
+    x = ones(n, 1);
+    y = ones(m, 1);
+    z = ones(n, 1);
     
-    % Find vector gradient 
-    grad_xyz = -grad_F \ F;
-
-    % Find maximum z_i * x_i
-    limit = delta * beta;
-    limit_vect = ones(n, 1) .* limit;
+    % Compile vector
+    xyz = [x; y; z];
     
-    % Choose alpha
-    h = 0.00001;
-    alpha = 0;
-    in_nbhd = true;
-    while alpha <= 1 && in_nbhd 
-
-        % Calculate "step"
-        xyz_new = xyz + alpha .* grad_xyz;
-
-        % Check if new vector is in neighborhood
-        in_nbhd = all(xyz_new(1:n, 1) .* xyz_new(n+m+1:n+m+n) ...
-                      >= limit_vect);
-
-        if in_nbhd
-            xyz = xyz_new;
-            alpha = alpha + h;
+    % Iterate until done
+    finished = false;
+    k = 0;
+    while ~finished
+        
+        % Iterate trial
+        k = k + 1;
+    
+        % Get epsilon value based on parity
+        eps = mod(k, 2) * eps_min + abs(1 - mod(k, 2)) * eps_max;
+    
+        % Find avg complementarity
+        beta = (z' * x) / n;
+    
+        % Find F of current vector subject to beta-epsilon
+        F = [-Q * x - c + A' * y + z;
+             A * x - b;
+             (z .* x) - (ones(n, 1) * eps * beta)];
+    
+        % Find gradient of F
+        grad_F = [-Q, A', eye(n);
+                  A, zeros(m, m), zeros(m, n);
+                  diag(z), zeros(n, m), diag(x)];
+        
+        % Find vector gradient 
+        grad_xyz = -grad_F \ F;
+    
+        % Find maximum z_i * x_i
+        limit = delta * beta;
+        limit_vect = ones(n, 1) .* limit;
+        
+        % Choose alpha
+        h = 0.00001;
+        alpha = 0;
+        in_nbhd = true;
+        while alpha <= 1 && in_nbhd 
+    
+            % Calculate "step"
+            xyz_new = xyz + alpha .* grad_xyz;
+    
+            % Check if new vector is in neighborhood
+            in_nbhd = all(xyz_new(1:n, 1) .* xyz_new(n+m+1:n+m+n) ...
+                          >= limit_vect);
+    
+            if in_nbhd
+                xyz = xyz_new;
+                alpha = alpha + h;
+            end
+        end
+    
+        % Calculate new vector
+        x = xyz(1:n, 1);
+        y = xyz(n+1:n+m, 1);
+        z = xyz(n+m+1:n+m+n);
+        
+        % Check terminating condition
+        finished = norm([-Q * x - c + A' * y + z;
+                         A * x - b;
+                         (z .* x) - (ones(n, 1) * eps * beta)]) < .000001;
+        
+        % Debugging
+        if show
+            disp(k)
+            disp(norm([-Q * x - c + A' * y + z;
+                       A * x - b;
+                       (z .* x) - (ones(n, 1) * eps * beta)]))
+        disp('------')
         end
     end
-
-    % Calculate new vector
-    x = xyz(1:n, 1);
-    y = xyz(n+1:n+m, 1);
-    z = xyz(n+m+1:n+m+n);
-    
-    % Check terminating condition
-    finished = norm([-Q * x - c + A' * y + z;
-                     A * x - b;
-                     (z .* x) - (ones(n, 1) * eps * beta)]) < .000001;
-    
-    % Debugging
-    disp(k)
-    % disp([x; y; z])
-    disp(norm([-Q * x - c + A' * y + z;
-                     A * x - b;
-                     (z .* x) - (ones(n, 1) * eps * beta)]))
-    disp('------')
 end
 
-%% Check solution
-[xI,fval,exitflag,output,lambda] = quadprog(Q,c,[],[],A,b,zeros(4,1),[inf inf inf inf]');
-zI=lambda.lower;
-yI=-lambda.eqlin;
-checkx=[-Q*x-c+A'*y+z; A*x-b; x.*z];
+% Run quadratic program
+[x, y, z, k] = QuadraticOptimization(Q, c, A, b, false)
+eps_min = 0.1
+eps_max = 0.9
+delta = 0.5
+
+% Turn off diary
+diary off
